baf91d7faea918171e9ba53fae28312c25ddf159
Joesh <joesh@MacBook-Pro.localdomain>
2020-11-25 17:04:37 -0700

    added components and sass file

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Footer from '../shared/Footer/Footer'
+import Header from '../shared/Header/Header'
+import './App.css'
+
+const App = () => (
+  <>
+    <Header />
+  
+    <Footer />
+  </>
+)
+
+export default App
---
+++ src/components/Musuem/Museum.js
@@ -0,0 +1,16 @@
+const Museum = () => {
+    
+}
+
+const test = {
+    "Create a functional component": {
+    "prefix": "fcomponent",
+    "body": [
+        "const ${TM_FILENAME_BASE} = () => {\n",
+        "}\n",
+        "export default ${TM_FILENAME_BASE}$1"
+    ]
+    },
+}
+
+export default Museum
