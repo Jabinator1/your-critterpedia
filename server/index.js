9044209bd0e681a59a90fe1343e5c07e8c7c9fd8
Jabinator1 <jabinator1@gmail.com>
2020-12-10 11:04:29 -0700

    setting up server to host

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import massive from "massive"
 import session from "express-session"
+import path from "path"
 import { loginUser, registerUser, logoutUser, getUser } from "./controllers/authController.js"
 import { editUser, editUserPass, deleteUser } from "./controllers/userController.js"
 import { checkUser } from "./middleware.js"
@@ -9,8 +10,10 @@ import { getCritterpedia, editCritterpedia } from "./controllers/critterpediaCon
 const app = express()
 dotenv.config()
 
+const __dirname = path.resolve(path.dirname(''))
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 
+app.use(express.static(`${__dirname}/build`))
 app.use(express.json())
 app.use(session({
     resave: false,
@@ -39,5 +42,9 @@ app.put("/api/user/password", checkUser, editUserPass)
 //# Critterpedia
 app.get("/api/critterpedia", checkUser, getCritterpedia)
 app.put("/api/critterpedia", checkUser, editCritterpedia)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/build/index.html"))
+})
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}.`))
