7d7b70df7caf1844f16ac5e8038cc3d8e811af72
Jabinator1 <jabinator1@gmail.com>
2020-12-12 20:51:12 -0700

    created grid layout for critterpedia

--- src/components/YourCritterpedia/CritterGraphs/CritterGraphs.js
+++ src/components/YourCritterpedia/CritterGraphs/CritterGraphs.js
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux"
-import bugsIcon from "../../../assets/bugsIcon.svg"
-import fishIcon from "../../../assets/fishIcon.svg"
-import seaCreaturesIcon from "../../../assets/seaCreaturesIcon.svg"
+import bugsIcon from "../../../assets/bugsIcon2.svg"
+import fishIcon from "../../../assets/fishIcon2.svg"
+import seaCreaturesIcon from "../../../assets/seaCreaturesIcon2.svg"
 import CritterGraph from "./CritterGraph/CritterGraph"
+import "./CritterGraphs.sass"
 
 const CritterGraphs = () => {
     const {insectsCritterpedia, fishCritterpedia, seaCritterpedia} = useSelector(state => state.critterpedia)
     return (
-        <div>
+        <div id="critter-graphs">
             <CritterGraph critterpediaLength={insectsCritterpedia.length} outOf={80} icon={bugsIcon}/>
             <CritterGraph critterpediaLength={fishCritterpedia.length} outOf={80} icon={fishIcon}/>
             <CritterGraph critterpediaLength={seaCritterpedia.length} outOf={40} icon={seaCreaturesIcon}/>
